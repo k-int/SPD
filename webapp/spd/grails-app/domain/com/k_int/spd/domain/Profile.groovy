24a84398161d6867cbb39bceda3d824cac6fb609
Ian Ibbotson <ianibbo@googlemail.com>
2011-09-07 15:32:18 +0100

    Installed nimble plugin

---
+++ webapp/spd/grails-app/domain/com/k_int/spd/domain/Profile.groovy
@@ -0,0 +1,8 @@
+
+package com.k_int.spd.domain
+
+class Profile extends grails.plugins.nimble.core.ProfileBase {
+
+	// Extend ProfileBase with your custom values here
+
+}
---
+++ webapp/spd/grails-app/domain/com/k_int/spd/domain/User.groovy
@@ -0,0 +1,8 @@
+
+package com.k_int.spd.domain
+
+class User extends grails.plugins.nimble.core.UserBase {
+
+	// Extend UserBase with your custom values here
+
+}
