59ee2b88a3699b307fc6cbb3d82863538370d8ed
Mark <Mark@K-Int.com>
2012-01-26 13:41:26 +0000

    added google analytics added formatting of time

--- webapp/spd/web-app/js/application.js
+++ webapp/spd/web-app/js/application.js
@@ -1,3 +1,17 @@
+/* start of google analytics code */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-17380719-11']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
+'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
+})();
+/* end of google analytics code */
+
 var Ajax;
 if (Ajax && (Ajax != null)) {
 	Ajax.Responders.register({
@@ -30,4 +44,7 @@ $(window).resize(function()
 $(document).ready(function()
 {	
 	resizeFrame();
+	
+	$('#footer-inner').prepend('<img src="' + CONTEXT_PATH + '/images/col_trust_small.png"/>');
 });
+
